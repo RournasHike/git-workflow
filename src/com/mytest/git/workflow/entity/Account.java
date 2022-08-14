f2aeb1822e504b1064a71538f5aec33423c95be4
liuqixin <liuqixin@qq.com>
2022-08-14 22:50:14 +0800

    account提交

--- src/com/mytest/git/workflow/entity/Account.java
+++ src/com/mytest/git/workflow/entity/Account.java
@@ -8,4 +8,22 @@ package com.mytest.git.workflow.entity;
  * @date 2022/8/6 18:50
  */
 public class Account {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
