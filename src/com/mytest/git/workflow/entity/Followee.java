48e79ef34dbd7d92539ea8217bb4382848079977
liuqixin <liuqixin@qq.com>
2022-08-14 22:52:02 +0800

    followee提交

--- src/com/mytest/git/workflow/entity/Followee.java
+++ src/com/mytest/git/workflow/entity/Followee.java
@@ -8,4 +8,13 @@ package com.mytest.git.workflow.entity;
  * @date 2022/8/6 18:50
  */
 public class Followee {
+    private Account account;
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
