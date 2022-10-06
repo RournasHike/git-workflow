bd2bbf72286b2e35c83d04a6e111bde9a5ed5cc2
liuqixin <liuqixin@qq.com>
2022-10-06 12:31:26 +0800

    [feature1229]提交issue330

--- src/com/mytest/git/workflow/branch/Issue330.java
+++ src/com/mytest/git/workflow/branch/Issue330.java
@@ -8,4 +8,23 @@ package com.mytest.git.workflow.branch;
  * @date 2022/8/6 18:48
  */
 public class Issue330 {
+
+    private String commitHash;
+    private String desc;
+
+    public String getCommitHash() {
+        return commitHash;
+    }
+
+    public void setCommitHash(String commitHash) {
+        this.commitHash = commitHash;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
