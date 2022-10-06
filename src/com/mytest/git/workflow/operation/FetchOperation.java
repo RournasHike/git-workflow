c53d19d4b83feb99102c53e8a82216c621852beb
liuqixin <liuqixin@qq.com>
2022-10-06 17:38:44 +0800

    [feature502]git operation fetch

---
+++ src/com/mytest/git/workflow/operation/FetchOperation.java
@@ -0,0 +1,11 @@
+package com.mytest.git.workflow.operation;
+
+/**
+ * @author Alex
+ * @version 1.0.0
+ * @company
+ * @copyright (c)   Inc. All rights reserved.
+ * @date 2022/10/6 17:37
+ */
+public class FetchOperation extends GitOperation{
+}
