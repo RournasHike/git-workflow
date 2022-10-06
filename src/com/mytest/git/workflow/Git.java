2f2eb7ed7f7337379d29f547606dceb41cd3829b
liuqixin <liuqixin@qq.com>
2022-10-06 17:12:22 +0800

    [master] Git注释

--- src/com/mytest/git/workflow/Git.java
+++ src/com/mytest/git/workflow/Git.java
@@ -1,12 +1,6 @@
 package com.mytest.git.workflow;
 
-import com.mytest.git.workflow.branch.Branch;
-import com.mytest.git.workflow.point.Head;
-
-import java.util.List;
-
 /**
- * git is a very useful version control system
  * @author Alex
  * @version 1.0.0
  * @company
@@ -14,6 +8,6 @@ import java.util.List;
  * @date 2022/8/6 18:58
  */
 public class Git {
-    private Head head;
-    private Branch currentBranch;
+    // TODO begin from branch master
+    // So do some operation
 }
