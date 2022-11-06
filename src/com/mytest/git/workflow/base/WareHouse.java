8187d727522f48db1e7a2a96735e50a5e82d3b54
liuqixin <liuqixin@qq.com>
2022-11-06 23:32:09 +0800

    [feature] 添加仓库特性

---
+++ src/com/mytest/git/workflow/base/WareHouse.java
@@ -0,0 +1,13 @@
+package com.mytest.git.workflow.base;
+
+/**
+ * 代码仓库
+ * @author Alex
+ * @version 1.0.0
+ * @company
+ * @copyright (c)   Inc. All rights reserved.
+ * @date 2022/11/6 23:30
+ */
+public abstract class WareHouse {
+
+}
