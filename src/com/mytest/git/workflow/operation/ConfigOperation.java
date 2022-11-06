3ee6e02c21784658a79e0669eaf579943a6ecf86
liuqixin <liuqixin@qq.com>
2022-11-06 23:09:08 +0800

    [master] fix cherry-pick conflict

---
+++ src/com/mytest/git/workflow/operation/ConfigOperation.java
@@ -0,0 +1,12 @@
+package com.mytest.git.workflow.operation;
+
+/**
+ * config
+ * @author Alex
+ * @version 1.0.0
+ * @company
+ * @copyright (c)   Inc. All rights reserved.
+ * @date 2022/10/6 17:57
+ */
+public class ConfigOperation extends GitOperation{
+}
