5fbb89f4f793cad246789925e2633a7dc96fb4d6
liuqixin <liuqixin@qq.com>
2022-12-02 13:40:47 +0800

    [feature20221201] Fork、Issue编码

--- src/com/mytest/git/workflow/entity/Fork.java
+++ src/com/mytest/git/workflow/entity/Fork.java
@@ -9,4 +9,30 @@ package com.mytest.git.workflow.entity;
  * @date 2022/8/6 18:49
  */
 public class Fork {
+    private String icon;
+    private Integer number;
+
+    public Fork() {
+    }
+
+    public Fork(String icon, Integer number) {
+        this.icon = icon;
+        this.number = number;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
 }
