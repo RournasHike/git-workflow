b0e6a12e26561cbaf532361d4945291c0a21fb06
liuqixin <liuqixin@qq.com>
2022-12-02 11:30:18 +0800

    [feature] 添加分支

---
+++ src/com/mytest/git/workflow/branch/Hotfix.java
@@ -0,0 +1,11 @@
+package com.mytest.git.workflow.branch;
+
+/**
+ * @author Alex
+ * @version 1.0.0
+ * @company
+ * @copyright (c)   Inc. All rights reserved.
+ * @date 2022/12/2 11:28
+ */
+public class Hotfix {
+}
