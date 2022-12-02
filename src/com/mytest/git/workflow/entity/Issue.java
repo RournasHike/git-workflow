80396a78d402d682e5567fd66fc9a61d0ebed192
liuqixin <liuqixin@qq.com>
2022-12-02 13:40:47 +0800

    [feature20221201] Fork、Issue编码

--- src/com/mytest/git/workflow/entity/Issue.java
+++ src/com/mytest/git/workflow/entity/Issue.java
@@ -9,4 +9,40 @@ package com.mytest.git.workflow.entity;
  * @date 2022/8/6 18:50
  */
 public class Issue {
+    private String issueNo;
+    private String desc;
+    private String branch;
+
+    public Issue() {
+    }
+
+    public Issue(String issueNo, String desc, String branch) {
+        this.issueNo = issueNo;
+        this.desc = desc;
+        this.branch = branch;
+    }
+
+    public String getIssueNo() {
+        return issueNo;
+    }
+
+    public void setIssueNo(String issueNo) {
+        this.issueNo = issueNo;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
 }
