ceab1b443bc43843d05864cc7dbde86d8e0dc4c7
liuqixin <liuqixin@qq.com>
2022-12-02 13:44:19 +0800

    [feature20221202] PullRequest编码

--- src/com/mytest/git/workflow/entity/PullRequest.java
+++ src/com/mytest/git/workflow/entity/PullRequest.java
@@ -9,4 +9,40 @@ package com.mytest.git.workflow.entity;
  * @date 2022/8/6 18:50
  */
 public class PullRequest {
+    private String targetBranch;
+    private String assignee;
+    private String commitHash;
+
+    public PullRequest() {
+    }
+
+    public PullRequest(String targetBranch, String assignee, String commitHash) {
+        this.targetBranch = targetBranch;
+        this.assignee = assignee;
+        this.commitHash = commitHash;
+    }
+
+    public String getTargetBranch() {
+        return targetBranch;
+    }
+
+    public void setTargetBranch(String targetBranch) {
+        this.targetBranch = targetBranch;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getCommitHash() {
+        return commitHash;
+    }
+
+    public void setCommitHash(String commitHash) {
+        this.commitHash = commitHash;
+    }
 }
